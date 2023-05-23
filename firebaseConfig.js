a1289b4803ebe46a98f652d716f360aa7e8c8fd8
NickB521 <104464307+NickB521@users.noreply.github.com>
2023-05-23 16:13:29 -0400

    no API key for u

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,13 +8,6 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsbYhMgPHYeLXwhjc64ZC6y6SSwkp8i04",
-  authDomain: "react-native-expo-1e170.firebaseapp.com",
-  projectId: "react-native-expo-1e170",
-  storageBucket: "react-native-expo-1e170.appspot.com",
-  messagingSenderId: "1054144152048",
-  appId: "1:1054144152048:web:a2e1f2dcb79aa5065edd35",
-  measurementId: "G-SHJFRNTP59"
 };
 
 const app = initializeApp(firebaseConfig);
